21a22a9376eb9517df9c9325b1553551c6b740d9
kumarparth-at-354110099186 <kumar.parth@blueorca.co>
2020-03-29 17:18:27 +0530

    Updates

---
+++ precache-manifest.143631ba568b6f6ff8a3b9df4af649eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d7854776a00bac39e340e7ec16591c",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "af84834f0215a9e073cf",
+    "url": "/e-shop/static/css/main.a8cabb45.chunk.css"
+  },
+  {
+    "revision": "679da44e7602a7c019d4",
+    "url": "/e-shop/static/js/2.f4eb87e4.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/e-shop/static/js/2.f4eb87e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af84834f0215a9e073cf",
+    "url": "/e-shop/static/js/main.1a57ce3a.chunk.js"
+  },
+  {
+    "revision": "0c34a726def52f3f3692",
+    "url": "/e-shop/static/js/runtime-main.221a5fc3.js"
+  },
+  {
+    "revision": "202bef233a3a3638cde6207085bae7f6",
+    "url": "/e-shop/static/media/cart.202bef23.svg"
+  },
+  {
+    "revision": "f0ebbc2e3ffa36eab469ec326c22e1ad",
+    "url": "/e-shop/static/media/shopping-bag.f0ebbc2e.svg"
+  }
+]);
