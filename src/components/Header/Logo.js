b0ae3860db37a8c2523fcbae6f45266f0b694b92
kumarparth-at-354110099186 <kumar.parth@blueorca.co>
2020-03-29 18:36:39 +0530

    changed the logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
     return (
         <div className="p-2">
-            <Link to="/"><img src={require('../../assets/shopping-bag.svg')} height="60px" alt="logo-img" />
+            <Link to="/"><img src={require('../../assets/logo.png')} height="60px" alt="logo-img" />
             </Link>
         </div>
     )
