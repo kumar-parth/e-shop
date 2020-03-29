34595b2cd1d6658a671d2578b82108e418da5a5a
kumarparth-at-354110099186 <kumar.parth@blueorca.co>
2020-03-29 18:46:55 +0530

    changed the logo

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-//import { setCartItems } from '../../actions';
+import '../css/cart.css';
 
 class CartWithBadgeIcon extends Component {
     render() {
         return (
             <div className="" style={{ alignSelf: "center" }}>
                 <Link to={'/cart'}>
-                    <button className="d-inline" style={{ border: 'none', background: 'none' }}>
+                    <button className="d-inline cartIcon__button">
                         <img alt="cart_icon" height={this.props.height ? this.props.height : 20} src={require('../assets/icons/cart.svg')} className="px-2" />
                         <span
-                            className="badge badge-danger rounded-circle"
-                            style={{
-                                fontSize: '11px',
-                                position: 'relative',
-                                right: '12px',
-                                top: '-3px',
-                                verticalAlign: 'top',
-                                paddingTop: '2px'
-                            }}
+                            className="badge badge-danger rounded-circle cartIcon__span"
                         >
                             {this.props.quantity > 0 ? this.props.quantity : null}
                         </span>
