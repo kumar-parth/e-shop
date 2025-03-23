7e39c00a57d158eae518d2f0744a414633f519f5
Kumar Parth <nmkp433@L-RMH5QG461Y.local>
2025-03-23 11:47:09 +0530

    fixed some issues

---
+++ build/precache-manifest.bc97e9677f66ec7605e06dfe0010c8a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410c921a7a0ba60d22499d09eb3a4516",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "23a503088bd15756576c",
+    "url": "/e-shop/static/css/main.d7cd38fc.chunk.css"
+  },
+  {
+    "revision": "62f9a195287677bc8c41",
+    "url": "/e-shop/static/js/2.caf2bfd2.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/e-shop/static/js/2.caf2bfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a503088bd15756576c",
+    "url": "/e-shop/static/js/main.96637e8e.chunk.js"
+  },
+  {
+    "revision": "0c34a726def52f3f3692",
+    "url": "/e-shop/static/js/runtime-main.221a5fc3.js"
+  },
+  {
+    "revision": "b90c84633325fbfc6ed110ab459216bd",
+    "url": "/e-shop/static/media/logo.b90c8463.png"
+  }
+]);
